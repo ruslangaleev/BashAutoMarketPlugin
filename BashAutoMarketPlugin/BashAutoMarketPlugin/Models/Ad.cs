2493c808e502242ba7e7ebd87af12a274973498b
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2017-08-10 10:28:19 +0500

    Initial

---
+++ BashAutoMarketPlugin/BashAutoMarketPlugin/Models/Ad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BashAutoMarketPlugin.Models
+{
+    public class Ad
+    {
+        public string Name { get; set; }
+    }
+}
